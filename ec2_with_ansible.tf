6172a905114dcb3d3d33a9d9dcd18a9ec0a29dff
Sarah Bean <beansarah365@gmail.com>
2024-03-20 09:00:46 -0400

    Notes and Code Day 11 & 12

--- ec2_with_ansible.tf
+++ ec2_with_ansible.tf
@@ -46,7 +46,19 @@ resource "aws_default_security_group" "ec2" {
       prefix_list_ids  = []
       self             = false
       security_groups  = []
+    },
+      {
+      cidr_blocks      = ["172.31.0.0/16"]
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1" #All traffic
+      description      = "internal-ports"
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      self             = false
+      security_groups  = []
     }
+
   ]
   tags = local.tags
 }
@@ -126,5 +138,20 @@ resource "null_resource" "copy_playbooks" {
     }
     source      = "./playbooks/"
     destination = "/tmp/"
+  }
+}
+resource "null_resource" "copy_manifest" {
+  triggers = {
+    timestamp = timestamp()
+  }
+  provisioner "file" {
+    connection {
+      type        = "ssh"
+      host        = aws_instance.my_ubuntu_server.public_ip
+      user        = "ubuntu"
+      private_key = tls_private_key.keypair.private_key_pem
+    }
+    source = "./manifests/"
+    destination = "/tmp/manifests"
   }
 }
